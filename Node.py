95dcf268ab80bf23a28a8e7c6a85657e48911000
emejia01 <erikmej01@gmail.com>
2021-04-29 23:23:15 -0400

    Modify Node constructor

--- Node.py
+++ Node.py
@@ -1,16 +1,16 @@
 from hashlib import sha256
-from Protocol import *
+#from BlockChainProject.Protocol import *
 from datetime import datetime
 
 
 
 class Node:
 
-    def __init__(self, FirstName, LastName, Email):
+    def __init__(self, FirstName, LastName, Email, UID=sha256(str(datetime.now()).encode("UTF-8")).hexdigest(), balance=10):
         self.FirstName = FirstName
         self.LastName = LastName
         self.Email = Email
-        self.UID = sha256(datetime.now().encode("UTF-8")).hexdigest()
+        self.UID = UID
         self.balance = 0
         self.Blockchain = [] # TODO: Pull from GCP
 
