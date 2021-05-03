71372fe51c396bb2dd1da1772fcfeb6afd26f5f3
emejia01 <erikmej01@gmail.com>
2021-05-03 00:12:03 -0400

    Add methods to return all Transactions in Mempool and to create a Mempool of random size

--- Miner.py
+++ Miner.py
@@ -1,8 +1,12 @@
 from BlockChainProject.Node import Node
-#import Protocol
+from google.cloud import datastore
 from datetime import datetime
 from hashlib import sha256
+from random import randint
+import os
 
+# Create OS environment variable for GCP Credential
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/erikmejia/Desktop/blockchainproject-311018-0932eb94714c.json"
 
 class Miner(Node):
 
@@ -11,14 +15,38 @@ class Miner(Node):
         self.tempMemPool = [] # memPool that is going to be mined.
         super(Miner, self).__init__(FirstName, LastName, Email, UID, balance)
 
+    # [transactionUID, self.UID, recieverID, self.balance]
+    def getMempool(self):
+        # get all transactions from Mempool table in GCP
+        client = datastore.Client()
+        query = client.query(kind="Mempool")
+        results = list(query.fetch())
 
-    # TODO: GetMempool():
-    #  going to be run every iteration of the mine() loop. List of "transactions" its rerally just a list of lists in the format:
-    #   [UID, senderID, RecieverID, Amouont, Fee]
+        allTransactions = []
+        for result in results:
+            result = dict(result)
+            transactionUID = result["UID"]
+            senderUID = result["senderID"]
+            recieverID = result["recieverID"]
+            transactionAmount = result["amount"]
 
+            currentTransaction = [transactionUID, senderUID, recieverID, transactionAmount]
+            allTransactions.append(currentTransaction)
 
+        return allTransactions
 
-    # make it so miners select from 10 - 15 transactions
+    # Creates temp mempool of random size of 10 - 15 transactions
+    def createRandomMempool(self):
+        allTransactions = self.getMempool()
+        if len(allTransactions) < 10:
+            return allTransactions
+
+        randomSize = randint(10, 15)
+        if len(allTransactions) < randomSize:
+            return allTransactions
+        else:
+            return allTransactions[:randomSize]
+
     # make it so a miners hash rate updates every 5 blocks
     def mine(self):
         return
