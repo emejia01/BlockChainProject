f161780f7c93b05bdf39fe3fb0600385eede35b4
TheodossisManavazian <tmanavazian01@manhattan.edu>
2021-05-05 12:05:50 -0400

    Added Visualizer screen

--- Node.py
+++ Node.py
@@ -1,5 +1,4 @@
 from hashlib import sha256
-#from BlockChainProject.Protocol import *
 from datetime import datetime
 from google.cloud import datastore
 import os
@@ -10,13 +9,14 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/erikmejia/Desktop/blockch
 
 class Node:
 
-    def __init__(self, FirstName, LastName, Email, UID=sha256(str(datetime.now()).encode("UTF-8")).hexdigest(), balance=10):
+    def __init__(self, FirstName ='', LastName='', Email='', UID=sha256(str(datetime.now()).encode("UTF-8")).hexdigest(),balance = 1):
         self.FirstName = FirstName
         self.LastName = LastName
         self.Email = Email
         self.UID = UID
-        self.balance = 0
+        self.balance = balance
         self.Blockchain = [] # TODO: Pull from GCP
+
 
 
     def verify(self, block):
@@ -43,11 +43,24 @@ class Node:
             "UID": transactionUID,
             "senderID": self.UID,
             "recieverID": recieverID,
-            "amount": self.balance,
-            #"fee": blockObj.previousHash,
-            "data": [transactionUID, self.UID, recieverID, self.balance]
+            "amount": 1,
+            "fee": .02,
+            "data": [transactionUID, self.UID, recieverID, 1, .02]
         })
         client.put(entity)
 
         # Update Node balance
-        self.balance = 0
+        #self.balance = 0 # we update only when the block that has this transaction is mined
+    def toString(self):
+        print("NODE:")
+        print('FirstName', self.FirstName)
+        print('LastName', self.LastName)
+        print('Email', self.Email)
+        print('UID', self.UID)
+        print('Bal',self.balance)
+
+
+
+
+#keep here
+from BlockChainProject.Protocol import *
